c33679469bcf981e1756ea4526c05635450056e8
Sumanth Tangirala <tangiralasumanth@gmail.com>
2023-11-09 20:30:39 -0500

    Added Readme

---
+++ README.md
@@ -0,0 +1,5 @@
+## Mobile Robot MuJoCo Environments
+
+Environments files are present in the `envs` directory. The `factory.py` file registers each of these environments and provides them the corresponding arguments.
+
+The `train.py` file has been provided for an example of how to use these files and environments
--- train.py
+++ train.py
@@ -1,64 +1,72 @@
-import numpy as np 
+import argparse
+
+import numpy as np
 from factory import MushrEnvironmentFactory
-
-
-env_factory = MushrEnvironmentFactory(
-    max_speed=0.5,
-    max_steering_angle=0.5,
-    max_steps=100,
-    prop_steps=10,
-    goal_limits=[0, 5],
-    with_obstacles=True,
-)
-
-env_name = "X2Obs"
-
-checkpt_path = "./trained_models/"+env_name+"_0_5_100_10_obstacles_sac/"
-load_path = "./trained_models/"+env_name+"_0_5_100_10_sac/"
-
-load_model = True
-train_model = True
-
-# env_factory.register_environments_with_position_and_orientation_goals()
-env_factory.register_environments_with_position_goals()
-
-from stable_baselines3 import HER, SAC
-import gym
-import os
-np.set_printoptions(suppress=True)
-
-env = gym.make(env_name+"Env-v0")
-
-breakpoint()
-alg = SAC
-num_steps = int(3e6)
-
-model = HER('MlpPolicy', env, alg, n_sampled_goal=4,
-            goal_selection_strategy='future',
-            verbose=1, buffer_size=int(1e6),
-            learning_rate=1e-3,
-            gamma=0.95, batch_size=256,max_episode_length=env.max_steps)
 
 from stable_baselines3.common.callbacks import EvalCallback, CheckpointCallback, CallbackList
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.her import ObsDictWrapper
 
+from stable_baselines3 import HER, SAC
+import gym
+import os
 
-eval_env = DummyVecEnv([lambda: gym.make(env_name+'Env-v0')])
-eval_env = ObsDictWrapper(eval_env)
+np.set_printoptions(suppress=True)
 
-if not os.path.exists(checkpt_path):
-    os.makedirs(checkpt_path)
 
-if train_model:
-    if load_model:
-        model = HER.load(load_path + "/best/best_model", env=env, verbose=1)
+def main(args):
+    env_factory = MushrEnvironmentFactory(
+        max_speed=0.5,
+        max_steering_angle=0.5,
+        max_steps=100,
+        prop_steps=10,
+        goal_limits=[0, 5],
+        with_obstacles=True,
+    )
+    num_steps = int(3e6)
 
-    checkpoint_callback = CheckpointCallback(save_freq=10000,save_path=checkpt_path)
-    eval_callback = EvalCallback(eval_env,n_eval_episodes=100,eval_freq=1e4,best_model_save_path=checkpt_path+"best/",
-                            log_path=checkpt_path+"logs/",deterministic=True)
+    env_name = args.env
+
+    checkpoint_path = "./trained_models/"+env_name
+
+    if args.has_orientation_goals:
+        env_factory.register_environments_with_position_and_orientation_goals()
+    else:
+        env_factory.register_environments_with_position_goals()
+
+    env = gym.make(env_name+"Env-v0")
+
+    model = HER('MlpPolicy', env, SAC, n_sampled_goal=4,
+                goal_selection_strategy='future',
+                verbose=1, buffer_size=int(1e6),
+                learning_rate=1e-3,
+                gamma=0.95, batch_size=256,max_episode_length=env.max_steps)
+
+    eval_env = DummyVecEnv([lambda: gym.make(env_name+'Env-v0')])
+    eval_env = ObsDictWrapper(eval_env)
+
+    if not os.path.exists(checkpoint_path):
+        os.makedirs(checkpoint_path)
+
+    checkpoint_callback = CheckpointCallback(save_freq=10000, save_path=checkpoint_path)
+    eval_callback = EvalCallback(
+        eval_env,
+        n_eval_episodes=100,
+        eval_freq=int(1e4),
+        best_model_save_path=checkpoint_path+"best/",
+        log_path=checkpoint_path+"logs/",
+        deterministic=True
+    )
+
     callback = CallbackList([checkpoint_callback,eval_callback])
-    model.learn(int(num_steps),callback=callback)
-    model.save(checkpt_path)
-else:
-    model = SAC.load(checkpt_path+"/best/best_model", env=env)
+
+    model.learn(num_steps, callback=callback)
+    model.save(checkpoint_path)
+
+
+if __name__ == '__main__':
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument('--env', type=str, default='X2Obs')
+    argParser.add_argument('--has_orientation_goals', default=False, action='store_true')
+
+    main(argParser.parse_args())
